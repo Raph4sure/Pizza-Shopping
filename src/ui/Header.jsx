dab5764df9f5cb48b4778b8d1612cd118c56483e
Raph <raph4sure007@gmail.com>
2025-04-13 23:38:17 +0100

    add order search functionality and implement data loading for order details

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,12 +1,15 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
 
 function Header() {
     return (
         <header>
             <Link to="/">Fast React Pizza Co.</Link>
+            <SearchOrder />
+
             <p>Just Testing</p>
         </header>
-    )
+    );
 }
 
-export default Header
+export default Header;
