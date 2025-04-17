632f5436a57e4d12a23f57842b48de0f33301ce2
Raph <raph4sure007@gmail.com>
2025-04-17 23:23:24 +0100

    implement cart functionality with clear and delete item features; add EmptyCart component for empty state handling

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,9 +1,12 @@
 import LinkButton from '../../ui/LinkButton';
 import Button from '../../ui/Button';
 import CartItem from './CartItem';
-import { useSelector } from 'react-redux';
+import EmptyCart from './EmptyCart'
+import { useDispatch, useSelector } from 'react-redux';
+import { clearCart, getCart } from './cartSlice';
 
-const fakeCart = [
+
+/* const fakeCart = [
   {
     pizzaId: 12,
     name: 'Mediterranean',
@@ -25,11 +28,15 @@ const fakeCart = [
     unitPrice: 15,
     totalPrice: 15,
   },
-];
+]; */
 
 function Cart() {
    const username = useSelector((state) => state.user.username);
-  const cart = fakeCart;
+  const cart = useSelector(getCart);
+
+  const dispatch = useDispatch()
+  
+  if(cart.length === 0) return <EmptyCart/>
 
   return (
     <div className='px-4 py-3'>
@@ -38,13 +45,13 @@ function Cart() {
       <h2 className='mt-7 text-xl font-semibold'>Your cart, { username }</h2>
 
       <ul className='mt-3 divide-y divide-stone-200 border-b'>
-        {cart.map((item) => (<CartItem item={item} key={item.key}/> ))}
+        {cart.map((item) => (<CartItem item={item} key={item.id}/> ))}
       </ul>
 
       <div className='mt-4 space-x-2'>
         <Button to="/order/new" type="primary">Order pizzas</Button>
 
-        <Button type="secondary">Clear Cart</Button>
+        <Button type="secondary" onClick={()=> dispatch(clearCart())}>Clear Cart</Button>
         
       </div>
     </div>
